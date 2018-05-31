4b04f0a7cbce1769d41ab63be05e09e2830a4220
Steve Kemp <steve@steve.org.uk>
2018-05-31 10:15:21 +0300

    Initial import

---
+++ runstats.go
@@ -0,0 +1,42 @@
+package runstats
+
+import (
+	"fmt"
+	"os"
+	"path"
+	"strconv"
+
+	graphite "github.com/marpaia/graphite-golang"
+	"github.com/skx/golang-metrics/collector"
+)
+
+var g *graphite.Graphite
+
+func init() {
+	host := os.Getenv("METRICS")
+
+	if host != "" {
+		fmt.Printf("HOST: %s\n", host)
+		var err error
+		g, err = graphite.NewGraphite(host, 2003)
+		fmt.Printf("%v\n", err)
+		if err == nil {
+			go runCollector()
+		} else {
+			fmt.Printf("Error setting up metrics: %s\n", err.Error())
+		}
+	}
+}
+
+func runCollector() {
+	prefix := "go." + path.Base(os.Args[0]) + "."
+
+	gaugeFunc := func(key string, val uint64) {
+		if g != nil {
+			fmt.Printf("%s ->%s\n", prefix+key, strconv.FormatUint(val, 10))
+			g.SimpleSend(prefix+key, strconv.FormatUint(val, 10))
+		}
+	}
+	c := collector.New(gaugeFunc)
+	c.Run()
+}
